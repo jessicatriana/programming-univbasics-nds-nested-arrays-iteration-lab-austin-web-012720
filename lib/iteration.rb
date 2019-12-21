cff1ae201340b14f699d8b8a29ecdba21cd58ae0
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-21 02:50:27 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,7 +42,7 @@ end
 i += 1
 
 end
-puts total
+total
 end
 #   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
 #   # if both numbers in the pair are even, then add both those numbers to the
